5c4bf9257adeefeab24d3e3d525f01be25d2b5b0
Borna Pavlić <borna.pavlic@yahoo.com>
2021-08-28 15:29:36 +0200

    added header (Navbar) to Header.js

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,7 +1,23 @@
 import React from "react";
+import { Navbar, Nav, Container, Row } from "react-bootstrap";
 
 const Header = () => {
-  return <header></header>;
+  return (
+    <header>
+      <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
+        <Container>
+          <Navbar.Brand href="/">Ice Eurotransport</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#home">Cart</Nav.Link>
+              <Nav.Link href="#link">Login</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  );
 };
 
 export default Header;
