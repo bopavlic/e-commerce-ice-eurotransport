c6f1a7d41c8364f942cd1446b70ca0f8683fc7be
Borna Pavlić <borna.pavlic@yahoo.com>
2021-08-28 17:46:15 +0200

    added product

---
+++ frontend/src/components/Product.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { Card } from "react-bootstrap";
+
+const Product = () => {
+  return <Card className="my-3 p-3 rounded">Product</Card>;
+};
+
+export default Product;
