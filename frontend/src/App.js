2ef688c33f7dff6144d1829724bf66ae7d5c51bc
Borna Pavlić <borna.pavlic@yahoo.com>
2021-08-28 15:16:51 +0200

    added bootstrap to footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,4 @@
+import { Container } from "react-bootstrap";
 import "./App.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
@@ -6,7 +7,11 @@ function App() {
   return (
     <div className="app">
       <Header />
-      <main></main>
+      <main className="py-3">
+        <Container>
+          <h1>Welcome</h1>
+        </Container>
+      </main>
       <Footer />
     </div>
   );
--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,7 +1,18 @@
 import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
 
 const Footer = () => {
-  return <footer></footer>;
+  return (
+    <footer>
+      <Container>
+        <Row>
+          <Col className="text-center py-3">
+            Copyright © 2021 Ice Eurotransport d.o.o. All rights reserved.
+          </Col>
+        </Row>
+      </Container>
+    </footer>
+  );
 };
 
 export default Footer;
