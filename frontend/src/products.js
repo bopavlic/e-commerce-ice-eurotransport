ea57f9d3deb67e6c647a4d6cdb74a5f9bf2e1e5a
Borna Pavlić <borna.pavlic@yahoo.com>
2021-08-28 17:23:35 +0200

    added products to products.js

---
+++ frontend/src/products.js
@@ -0,0 +1,53 @@
+const products = [
+  {
+    _id: 1,
+    name: "Drva",
+    image: "/images/drva.jpg",
+    description: "Drva za ogrijev u paleti",
+    price: 850,
+    countInStock: 10,
+  },
+  {
+    _id: 2,
+    name: "Euro Paleta",
+    image: "/images/euro-palet.jpg",
+    description: "Euro paleta nova",
+    price: 99,
+    countInStock: 100,
+  },
+  {
+    _id: 3,
+    name: "Jelova letva",
+    image: "/images/letve.jpg",
+    description:
+      "Jelova letva dimenzija (cm): 400 x 5 x 4. Cijena je za 1m^3. U kubiku se nalazi 125 komada dasaka. Moguće ostale dimenzije po narudžbi",
+    price: 3125,
+    countInStock: 10,
+  },
+  {
+    _id: 4,
+    name: "Potpala",
+    image: "/images/potpala.jpg",
+    description: "Drvo za potpalu u vreći",
+    price: 12,
+    countInStock: 150,
+  },
+  {
+    _id: 5,
+    name: "Skladište",
+    image: "/images/skladiste.jpg",
+    description:
+      "Usluga najma vanjskog i unutarnjeg skladišta s mogućnošču istovara/utovara robe na rampi. Nazvati za cijenu.",
+    price: 99999,
+    countInStock: 1000,
+  },
+  {
+    _id: 3,
+    name: "Transport",
+    image: "/images/transport.jpg",
+    description:
+      "Mogućnost transporta svih navedenih artikla, ali i prijevoz Vaše robe.",
+    price: 2500,
+    countInStock: 10,
+  },
+];
