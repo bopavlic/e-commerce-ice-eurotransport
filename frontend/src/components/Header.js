93a9b0f855cccf6ed9754db91e363de899171f37
Borna Pavlić <borna.pavlic@yahoo.com>
2021-08-28 19:10:20 +0200

    replaced href with link from react-router

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,21 +1,29 @@
 import React from "react";
+import "./Header.css";
 import { Navbar, Nav, Container } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
 
 const Header = () => {
   return (
     <header>
       <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
         <Container>
-          <Navbar.Brand href="/">Ice Eurotransport</Navbar.Brand>
+          <LinkContainer to="/">
+            <Navbar.Brand>Ice Eurotransport</Navbar.Brand>
+          </LinkContainer>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="mr-auto">
-              <Nav.Link href="#home">
-                <i className="fas fa-shopping-cart"></i>Cart
-              </Nav.Link>
-              <Nav.Link href="#link">
-                <i className="fas fa-user"></i>Login
-              </Nav.Link>
+              <LinkContainer to="#home">
+                <Nav.Link>
+                  <i className="fas fa-shopping-cart"></i>Cart
+                </Nav.Link>
+              </LinkContainer>
+              <LinkContainer to="#link">
+                <Nav.Link>
+                  <i className="fas fa-user"></i>Login
+                </Nav.Link>
+              </LinkContainer>
             </Nav>
           </Navbar.Collapse>
         </Container>
--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,27 +1,28 @@
 import React from "react";
 import { Card } from "react-bootstrap";
 import "./Product.css";
+import { Link } from "react-router-dom";
 
 const Product = ({ product }) => {
   return (
     //image
     <Card className="my-3 p-3 rounded">
-      <a href={`/product/${product._id}`}>
+      <Link to={`/product/${product._id}`}>
         <Card.Img src={product.image} />
-      </a>
+      </Link>
 
       {/* name */}
       <Card.Body>
-        <a href={`/product/${product._id}`}>
+        <Link to={`/product/${product._id}`}>
           <Card.Title as="div">
             <strong>{product.name}</strong>
           </Card.Title>
-        </a>
+        </Link>
 
         {/* price */}
         <Card.Text as="h3">
           <div className="my-3">
-            {product.price.toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ".")}
+            {product.price.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, "$&,")}
             kn
           </div>
         </Card.Text>
